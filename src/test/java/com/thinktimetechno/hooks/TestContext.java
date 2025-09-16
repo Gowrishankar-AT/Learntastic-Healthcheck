41488afdfbaf9d37b878c0146991f6ae7517c4fc
Gowrishankar-AT <shankar@thinktime.in>
2025-09-16 17:01:18 +0530

    Update TestContext.java

--- src/test/java/com/thinktimetechno/hooks/TestContext.java
+++ src/test/java/com/thinktimetechno/hooks/TestContext.java
@@ -3,7 +3,7 @@ package com.thinktimetechno.hooks;
 import com.thinktimetechno.driver.DriverManager;
 import com.thinktimetechno.driver.TargetFactory;
 //import com.thinktimetechno.projects.website.pages.DashboardPage;
-
+import com.thinktimetechno.constants.FrameworkConstants;
 import com.thinktimetechno.utils.LogUtils;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ThreadGuard;
@@ -16,10 +16,9 @@ public class TestContext {
     System.setProperty("webdriver.http.factory", "jdk-http-client");
     driver = ThreadGuard.protect(new TargetFactory().createInstance());
     
-    String headless = FrameworkConstants.HEADLESS;
-    if (!headless.equalsIgnoreCase("true")) {
-        driver.manage().window().maximize();
-    }
+    if (Boolean.valueOf(FrameworkConstants.HEADLESS) == false) {
+		driver.manage().window().maximize();
+		 }
 
     DriverManager.setDriver(driver);
     LogUtils.info("Driver in TestContext: " + getDriver());
